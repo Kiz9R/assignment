d1c71aeb505ec9c564b91d681830023df9e0c36a
kiz9r <ritankarxkiz9r@gmail.com>
2023-03-19 21:40:39 +0530

    added both person

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, React } from "react";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("Jhon");
+  const [age, setAge] = useState(21);
+  const [hobby, setHobby] = useState("Reading Books");
+
+  const displayPerson = () => {
+    setName("Bob");
+    setAge(30);
+    setHobby("Playing Chess");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{hobby}</h3>
+    </>
   );
-}
+};
 
 export default App;
